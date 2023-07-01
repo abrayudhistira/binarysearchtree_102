2cb3b118b015570ad8c0a94e906fea56e4a2ac2f
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-07-02 00:09:18 +0700

    make the left child of the parent point to the new node

--- binarysearchtree_102/binarysearchtree_102.cpp
+++ binarysearchtree_102/binarysearchtree_102.cpp
@@ -45,6 +45,9 @@ public:
 			return; // Exit
 		}
 		if (element < parent->info) // if the value in data field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; // make the left child of the parent point to the new node
 
+		}
 	}
 };
